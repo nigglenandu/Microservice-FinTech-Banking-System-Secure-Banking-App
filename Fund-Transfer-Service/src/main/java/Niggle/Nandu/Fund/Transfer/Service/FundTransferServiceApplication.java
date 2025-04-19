8618287e20c94d19eeac4c2bbe63b25fbe525f42
[Niggle Nandu] <nigglenandu@gmail.com>
2025-04-19 17:20:08 +0545

    yaml for fund transfer service

--- Fund-Transfer-Service/src/main/java/Niggle/Nandu/Fund/Transfer/Service/FundTransferServiceApplication.java
+++ Fund-Transfer-Service/src/main/java/Niggle/Nandu/Fund/Transfer/Service/FundTransferServiceApplication.java
@@ -2,10 +2,12 @@ package Niggle.Nandu.Fund.Transfer.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FundTransferServiceApplication {
 
 	public static void main(String[] args) {
