d59b6553a9f23a78062c9805c4c7b9b39d5b8337
[Niggle Nandu] <nigglenandu@gmail.com>
2025-04-20 22:59:50 +0545

    transaction

--- Transaction-History-Service/src/main/java/Niggle/Nandu/Transaction/History/Clients/AccountClient.java
+++ Transaction-History-Service/src/main/java/Niggle/Nandu/Transaction/History/Clients/AccountClient.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "Account-Management-Service", url = "http://Account-Management-Service/accounts/all")
+@FeignClient(name = "Account-Management-Service",
+        url ="${Account-Management-Service.url}")
 public interface AccountClient {
     @GetMapping("/accounts/{id}")
     AccountDto getAccountById(@PathVariable("id") Long id);
