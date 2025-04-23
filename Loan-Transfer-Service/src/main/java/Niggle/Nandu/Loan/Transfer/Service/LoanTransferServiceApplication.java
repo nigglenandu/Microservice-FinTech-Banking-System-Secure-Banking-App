10e3386c17c6e837da5427c0ac2010a95591006b
[Niggle Nandu] <nigglenandu@gmail.com>
2025-04-23 22:52:59 +0545

    zipkin docker

--- Loan-Transfer-Service/src/main/java/Niggle/Nandu/Loan/Transfer/Service/LoanTransferServiceApplication.java
+++ Loan-Transfer-Service/src/main/java/Niggle/Nandu/Loan/Transfer/Service/LoanTransferServiceApplication.java
@@ -2,8 +2,12 @@ package Niggle.Nandu.Loan.Transfer.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class LoanTransferServiceApplication {
 
 	public static void main(String[] args) {
