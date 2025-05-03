10f192d9afee0233c1ad3963fe58cb7a0471d83e
[Niggle Nandu] <nigglenandu@gmail.com>
2025-05-03 22:42:28 +0545

    rabbitmq config

---
+++ Account-Management-Service/src/main/java/Niggle/Nandu/Account/Management/Service/messaging/RabbitMQConfiguration.java
@@ -0,0 +1,32 @@
+package Niggle.Nandu.Account.Management.Service.messaging;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.amqp.core.Queue;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+    public static final String QUEUE = "fund_transfer_queue";
+    public static final String EXCHANGE = "banking_exchange";
+    public static final String ROUTING_KEY = "banking_routing_key";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+}
