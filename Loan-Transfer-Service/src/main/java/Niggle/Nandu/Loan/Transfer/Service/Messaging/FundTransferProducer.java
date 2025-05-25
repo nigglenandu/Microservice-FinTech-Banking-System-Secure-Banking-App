343b79f09504b547bf77a342d8d87431786e6591
[Niggle Nandu] <nigglenandu@gmail.com>
2025-05-25 13:49:13 +0545

    fundtransfer producer

---
+++ Loan-Transfer-Service/src/main/java/Niggle/Nandu/Loan/Transfer/Service/Messaging/FundTransferProducer.java
@@ -0,0 +1,20 @@
+package Niggle.Nandu.Loan.Transfer.Service.Messaging;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FundTransferProducer {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendTransferMessage(String message){
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.EXCHANGE_NAME,
+                RabbitMQConfig.FUND_TRANSFERS_ROUTING_KEY,
+                message
+        );
+        System.out.println("Send fund transfer message to RabbitMQ: " + message);
+    }
+}
